65e1e2e7131f04821e7afe5e91005946f8a092c0
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-10-30 12:56:33 -0600

    update: manage product insertion in index.html

--- client/src/js/controllers/index.js
+++ client/src/js/controllers/index.js
@@ -1,9 +1,16 @@
 import { loadTemplate } from "/e-commerce/client/src/js/utils/helpers.js";
+import { products } from "/e-commerce/client/src/js/utils/constants.js";
+import { productCard } from "/e-commerce/client/src/js/components/productCard.js";
 
 const header = document.getElementById("header");
 const headerUrl = `src/templates/header.html`;
 const footer = document.getElementById("footer");
 const footerUrl = `src/templates/footer.html`;
+const productsContainer = document.getElementById("products-container");
 
 loadTemplate(headerUrl, header);
 loadTemplate(footerUrl, footer);
+
+products.forEach((product) => {
+  productsContainer.innerHTML += productCard(product);
+})
