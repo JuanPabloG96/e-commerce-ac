850667d096d5fba4d9f0c4c011ad4e4b723ea838
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-10-30 12:55:26 -0600

    add: test dictionary for pruducts

---
+++ client/src/js/utils/constants.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    "id": 1,
+    "nombre": "Osito Feliz",
+    "precio": 19.99,
+    "imagen": "assets/products/oso.webp",
+    "descripcion": "Un adorable osito feliz para abrazar."
+  },
+  {
+    "id": 2,
+    "nombre": "Conejito Suave",
+    "precio": 24.99,
+    "imagen": "assets/products/conejito-suave.webp",
+    "descripcion": "Un conejito suave y tierno, perfecto para niños."
+  },
+  {
+    "id": 3,
+    "nombre": "Gatito Dormilón",
+    "precio": 22.99,
+    "imagen": "assets/products/gato.webp",
+    "descripcion": "Un gatito dormilón que siempre está listo para descansar."
+  },
+  {
+    "id": 4,
+    "nombre": "Perrito Juguetón",
+    "precio": 27.99,
+    "imagen": "assets/products/perro.webp",
+    "descripcion": "Un perrito juguetón que trae diversión a casa."
+  }
+]
